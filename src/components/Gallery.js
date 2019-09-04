d655e3a1753eba8e7965f8d7c22ab7643c26e28e
MrNivorous <karn.sood@gmail.com>
2019-09-04 11:32:10 +0700

    need to fix gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,21 +4,21 @@ class Gallery extends React.Component {
 	render () {
 		return (
 			<div className="gallery">
-				<div className="image-main" style={{backgroundImage: "url('https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg')"}}>
+				<div className="image-main" style={{backgroundImage: `url(${this.props.main.image})`}}>
 					<button className="icon">
 						<i className="far fa-heart"></i>
 					</button>
 				</div>
 				<div className="thumbnails">
-					<div className="thumbnail selected" style={{backgroundImage: "url('https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg')"}}></div>
-					<div className="thumbnail" style={{backgroundImage: "url('https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg')"}}></div>
-					<div className="thumbnail" style={{backgroundImage: "url('https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223174.jpg')"}}></div>
-					<div className="thumbnail" style={{backgroundImage: "url('https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223178.jpg')"}}></div>
-					<div className="thumbnail" style={{backgroundImage: "url('https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223180.jpg')"}}></div>
-					<div className="thumbnail" style={{backgroundImage: "url('https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223186.jpg')"}}></div>
-					<div className="thumbnail" style={{backgroundImage: "url('https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223190.jpg')"}}></div>
-					<div className="thumbnail" style={{backgroundImage: "url('https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223195.jpg')"}}></div>
-					<div className="thumbnail" style={{backgroundImage: "url('https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223199.jpg')"}}></div>
+					<div className="thumbnail selected" style={{backgroundImage: `url(${this.props.main.image})`}}></div>
+					<div className="thumbnail" style={{backgroundImage: `url(${this.props.main.image})`}}></div>
+					<div className="thumbnail" style={{backgroundImage: `url(${this.props.main.image})`}}></div>
+					<div className="thumbnail" style={{backgroundImage: `url(${this.props.main.image})`}}></div>
+					<div className="thumbnail" style={{backgroundImage: `url(${this.props.main.image})`}}></div>
+					<div className="thumbnail" style={{backgroundImage: `url(${this.props.main.image})`}}></div>
+					<div className="thumbnail" style={{backgroundImage: `url(${this.props.main.image})`}}></div>
+					<div className="thumbnail" style={{backgroundImage: `url(${this.props.main.image})`}}></div>
+					<div className="thumbnail" style={{backgroundImage: `url(${this.props.main.image})`}}></div>
 				</div>
 			</div>
 		)
--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -15,7 +15,7 @@ class Thumbnail extends React.Component {
 				<div className="content">
 					<small className="meta">{this.props.place.type}</small>
 					<h2>{this.props.place.title}</h2>
-					{this.props.place.location ? <small className="location">
+						{this.props.place.location ? <small className="location">
 															<i className="fas fa-map-marker-alt"></i>
 															<span>{this.props.place.location}</span>
 														</small> : null}
@@ -27,7 +27,7 @@ class Thumbnail extends React.Component {
 						<i className="fas fa-star"></i>
 						<i className="far fa-star"></i>
 						<span>{this.props.place.reviews}</span>
-						{this.props.place.date ? <span className="date">18 Apr 2019 - 27 Apr 2019</span> : null}
+						{this.props.place.date ? <span className="date">{this.props.place.date}</span> : null}
 
 					</span>
 				</div>
